ad615827e3e04152f18d24b18b5f53b5e97ec5f0
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-11-05 18:29:53 +0330

    Practice Image component in NextJs

--- project1/pages/products/books/index.js
+++ project1/pages/products/books/index.js
@@ -1,8 +1,23 @@
+import Link from 'next/link';
+import Image from "next/image"
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const BooksList = () => {
   return (
     <div>
-      BooksList
+      <h1>BooksList</h1>
+      {source.map((elem) => {
+        const imgSrc = `https://picsum.photos/id/${elem}/250/300`
+        return (
+          <div key={elem} >
+            <h5>book {elem}</h5>
+            <p><Link href={`/products/books/${elem}`} >Click to see details</Link> </p><br />
+            <Image src={imgSrc} loader={() => imgSrc} unoptimized
+              width="250" height="250" alt='' /><hr />
+          </div>
+        )
+      })}
     </div>
   )
 }
--- project1/pages/products/games/index.js
+++ project1/pages/products/games/index.js
@@ -1,7 +1,25 @@
+import Link from 'next/link';
+import Image from "next/image"
+
+const source = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
 
 const GamesList = () => {
   return (
-    <div>GamesList</div>
+    <div>
+      <h1>GamesList</h1>
+      {source.map((elem) => {
+        const imgSrc = `https://picsum.photos/id/${elem}/250/300`
+        return (
+          <div key={elem} >
+            <h5>game {elem}</h5>
+            <p><Link href={`/products/games/${elem}`} >Click to see details</Link> </p><br />
+            <Image src={imgSrc} loader={() => imgSrc} unoptimized
+              width="250" height="250" alt='' /><hr />
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
