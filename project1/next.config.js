dd3cb59de7b0e0504755bc4242826374d524ccab
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-11-06 20:20:22 +0330

    Practice next config and Redirect users

--- project1/next.config.js
+++ project1/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+
+  async redirects() {
+    return [
+      {
+        source: "/hi",
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: "/bye",
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
+
 }
 
 module.exports = nextConfig
